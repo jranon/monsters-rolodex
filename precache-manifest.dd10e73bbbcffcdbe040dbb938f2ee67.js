a57a5994415a3df79cb92a5876060304b82f138b
unknown <jesse.ranon@gmail.com>
2019-10-28 16:22:56 -0700

    Updates

---
+++ precache-manifest.dd10e73bbbcffcdbe040dbb938f2ee67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5feb1165ea732e849baf2c6824bc2d76",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2d6758e41085d7b55ef9",
+    "url": "/monsters-rolodex/static/css/main.08809d53.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "2d6758e41085d7b55ef9",
+    "url": "/monsters-rolodex/static/js/main.5d8491ad.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
